f469b60612344740332fdc92e6bd916b575a676d
BTRON32 <131682089+BTRON32@users.noreply.github.com>
2025-06-06 20:08:45 +0530

    Update FoodItem.jsx

--- src/components/FoodItem/FoodItem.jsx
+++ src/components/FoodItem/FoodItem.jsx
@@ -2,6 +2,7 @@ import React, { useContext } from 'react'
 import "./FoodItem.css"
 import {assets} from '../../assets/assets'
 import { StoreContext } from '../../context/StoreContext'
+import { toast } from 'react-toastify'
 
 const FoodItem = ({id,name,price,description,image}) => {
 
@@ -14,9 +15,9 @@ const FoodItem = ({id,name,price,description,image}) => {
         <div className="food-item-img-container">
             <img className='food-item-image' src={url+"/images/"+image} alt="" />
             {!cartItems[id]
-              ? <img onClick={()=>addToCart(id)} className='add' src={assets.add_icon_white} alt=''/>
+              ? <img onClick={()=>addToCart(id,toast.success("Food item added to cart"))} className='add' src={assets.add_icon_white} alt=''/>
               :<div className='food-item-counter'>
-                <img onClick={()=>removeFromCart(id)} src={assets.remove_icon_red} alt="" />
+                <img onClick={()=>removeFromCart(id,toast.success("Food item removed to cart"))} src={assets.remove_icon_red} alt="" />
                 <p>{cartItems[id]}</p>
                 <img onClick={()=>addToCart(id)} src={assets.add_icon_green} alt="" />
               </div>
